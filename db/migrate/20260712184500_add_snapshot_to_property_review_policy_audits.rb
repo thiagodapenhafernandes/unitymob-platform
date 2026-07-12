e5262a94c6aea1693713a97a246169fa4b16a24b
Thiago da P. Fernandes <thiagodap.fernandes@MacBook-Pro-de-Thiago.local>
2026-07-12 16:42:14 -0300

    Revert "feat: restore review policies as new versions"
    This reverts commit 348c6301b8cf58b3475f22190b19298eaa2d3998.

--- db/migrate/20260712184500_add_snapshot_to_property_review_policy_audits.rb
+++
@@ -1,5 +0,0 @@
-class AddSnapshotToPropertyReviewPolicyAudits < ActiveRecord::Migration[7.1]
-  def change
-    add_column :property_review_policy_audit_logs, :policy_snapshot, :jsonb, null: false, default: {}
-  end
-end
