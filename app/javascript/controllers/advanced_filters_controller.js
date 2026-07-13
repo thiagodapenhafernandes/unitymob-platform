85bcc27c23f86782c24e08fdf1f44fafe5a12d0d
Thiago da P. Fernandes <thiagodap.fernandes@MacBook-Pro-de-Thiago.local>
2026-07-13 17:03:39 -0300

    fix: prioritize mobile property results

--- app/javascript/controllers/advanced_filters_controller.js
+++ app/javascript/controllers/advanced_filters_controller.js
@@ -17,14 +17,20 @@ export default class extends Controller {
   }
 
   open() {
-    this.sidebarTarget.classList.remove('translate-x-full')
+    clearTimeout(this.hideTimer)
+    this.sidebarTarget.classList.remove('hidden')
     this.overlayTarget.classList.remove('hidden')
     document.body.classList.add('overflow-hidden')
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => this.sidebarTarget.classList.remove('translate-x-full'))
+    })
   }
 
   close() {
     this.sidebarTarget.classList.add('translate-x-full')
     this.overlayTarget.classList.add('hidden')
     document.body.classList.remove('overflow-hidden')
+    this.hideTimer = setTimeout(() => this.sidebarTarget.classList.add('hidden'), 300)
   }
 }
