fef6791304b98a296c9168128cf041148dc71430
Thiago da P. Fernandes <thiagodap.fernandes@MacBook-Pro-de-Thiago.local>
2026-08-11 20:20:36 -0300

    Atualiza deploy da Conexao para novo servidor

--- config/deploy/conexaoimobiliaria.rb
+++ config/deploy/conexaoimobiliaria.rb
@@ -2,9 +2,9 @@
 
 set :stage, "conexaoimobiliaria"
 set :application_name, "conexao_imobiliaria"
-set :domain, "157.245.253.175"
-set :user, "bc.imobiliariaconexao.com.br"
-set :deploy_to, "/home/bc.imobiliariaconexao.com.br/deploy"
+set :domain, "app.conexaobc.com"
+set :user, "conexao"
+set :deploy_to, "/home/conexao/deploy"
 
 set :puma_service, "puma_conexao_imobiliaria_production"
 set :solid_queue_service, "solid_queue_conexao_imobiliaria_production"
